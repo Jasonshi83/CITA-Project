9c5f777ed6154d277ae3d1e9a89f7240dd0e402c
Jason <shizong@outlook.com>
2019-12-04 15:38:11 +1300

    Category is working!

--- UpcomingEvent.php
+++ UpcomingEvent.php
@@ -13,19 +13,32 @@
     }
     $conn = mysqli_connect("10.140.61.247:3306", "citauser", "citauser", "cita_events") or die("could not connect to DB");
     //Test
-    $result = $conn->query("select * from events where EventStart >= CURRENT_TIMESTAMP order by eventStart;");
+//    $result = $conn->query("select * from events where EventStart >= CURRENT_TIMESTAMP order by eventStart;");
+    $result = $conn->query("select Events.id, Events.ShortDesc, Events.QRToken, Events.EventVideo, Events.EventStart, Events.EventEnd, Events.AttendeeLimit, Events.AllowGuests, Events.Category, EventsCategory.category as type FROM Events JOIN EventsCategory ON Events.Category = Eventscategory.id where Events.EventStart >= CURRENT_TIMESTAMP order by Events.EventStart;");
 //    $r = mysqli_fetch_assoc($sth)
 //    $rows = array();
     if($result){
         while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
+//        print_r ($row);
         $user = new User();
+//        $user->id = $row["Events.id"];
+//        $user->calendarId = $row["EventsCategory.category"];
+//        $user->title = $row["Events.ShortDesc"];
+////        $user->category = $row["Events.Category"];
+//        $user->category = 'allday';
+//        $user->dueDateClass = '';
+//        $user->start = $row["Events.EventStart"];
+//        $user->end = $row["Events.EventEnd"];
+
         $user->id = $row["id"];
-        $user->calendarId = '';
+        $user->calendarId = $row["type"];
         $user->title = $row["ShortDesc"];
-        $user->category = $row["Category"];
+        $user->category = $row["type"];
+//        $user->category = 'allday';
         $user->dueDateClass = '';
         $user->start = $row["EventStart"];
         $user->end = $row["EventEnd"];
+
         $data[] = $user;
         }
         $json = json_encode($data);
