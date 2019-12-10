367e37657edc03b564a1cd4a0337a54c21630584
Sam Lock <32768806+sdotlock@users.noreply.github.com>
2019-12-10 13:35:42 +1300

    commiting garbage

--- js/eventDetailsScript.js
+++ js/eventDetailsScript.js
@@ -29,10 +29,12 @@ function displayEvents(selectedCategory) {
             for (var i = 1; i < 4; i++) {
 
                 var titletext = schedules[i-1].title;
+                console.log("Samuel:" + titletext)
                 var titleDiv = document.getElementById(card-title+i).innerHTML = titletext
 
                 var start = "Start time: " + schedules[i-1].start;
                 var startTimeDiv = document.getElementById(card-text+i).innerHTML = start
+                
 
                 var locationdetail = schedules[i-1].address;
                 var locationdesc = document.getElementById(card-text+i+i).innerHTML = locationdetail
