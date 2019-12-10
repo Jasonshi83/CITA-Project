abe6776f334c4e7a0cf903584a96e91a5fc7a593
Sam Lock <32768806+sdotlock@users.noreply.github.com>
2019-12-10 20:42:59 +1300

    design all most done. loading from db lots

--- js/eventDetailsScript.js
+++ js/eventDetailsScript.js
@@ -1,9 +1,12 @@
 var selectedCategory;
 
 
-console.log("samuel samuel function start")
+
+getQueryVariable('id')
 displayEvents("myevents");
-console.log("samuel samuel function end")
+displayMainEvent(getQueryVariable('id'))
+
+
 
 function myFunction(category) {
 
@@ -14,21 +17,65 @@ function myFunction(category) {
     displayEvents(selectedCategory);
 }
 
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+
+
+
+function displayMainEvent(EventID) {
+    
+    //if (EventID != false) {
+
+        var oReq = new XMLHttpRequest();
+        
+        oReq.onload = function() {
+                
+        var schedules = JSON.parse(this.responseText);
+
+                var titletext = schedules[0].title
+                var titleDiv = document.getElementById("maintitle").innerHTML = titletext
+
+                var start = "Start time: " + schedules[0].start;
+                var startTimeDiv = document.getElementById("starttime").innerHTML = start
+
+                var desc = schedules[0].DetailedEventDesc;
+                var descDiv = document.getElementById("detailsdesc")
+                
+                var locationdetail = schedules[0].address;
+                var locationdesc = document.getElementById("mainaddress").innerHTML = locationdetail
+        
+        } 
+
+    oReq.open("get", "UpcomingEvent.php", true);
+    oReq.send();
+
+  //  } 
+    } 
+
+    
+
+
 
 function displayEvents(selectedCategory) {
-    console.log("inside display event")
+    
     var oReq = new XMLHttpRequest();
-        console.log("xml req")
+        
     oReq.onload = function() {
-        console.log("onreq onload")
+        
         var schedules = JSON.parse(this.responseText);
-        console.log("json parsed")
+        
         
             for (var i = 1; i < 4; i++) {
-                console.log("samuel samuel" + i)
+                                
                 var titletext = schedules[i-1].title;
-                console.log("Samuel:" + titletext)
-                console.log("Samuel:")
                 var titleDiv = document.getElementById("card-title"+i).innerHTML = titletext
 
                 var start = "Start time: " + schedules[i-1].start;
@@ -36,7 +83,16 @@ function displayEvents(selectedCategory) {
                 
                 var locationdetail = schedules[i-1].address;
                 var locationdesc = document.getElementById("card-texti"+i).innerHTML = locationdetail
-  
+                
+                var deck = document.getElementById("card"+i)
+                deck.addEventListener('click', function (e) {
+                var target = e.target
+
+                window.location.href = 'http://localhost/CITA-NZ2/CITA-Project/EventDetail.php?id=' + titletext;
+            
+                })
+
+                
             }
         } 
         console.log("display events finished")
